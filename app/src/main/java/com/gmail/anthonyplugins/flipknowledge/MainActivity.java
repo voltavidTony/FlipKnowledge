24958e587dc5a7d3572ea53b720c0214292f7f13
voltavidTony <34665710+voltavidTony@users.noreply.github.com>
2018-10-22 22:33:33 -0400

    Adjusted the answer colors (and removed animations again)

--- app/src/main/java/com/gmail/anthonyplugins/flipknowledge/MainActivity.java
+++ app/src/main/java/com/gmail/anthonyplugins/flipknowledge/MainActivity.java
@@ -1,81 +1,30 @@
 package com.gmail.anthonyplugins.flipknowledge;
 
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean answerShown = false;
-    private float scale;
-
-    private Animator rotate;
-    private Animator question;
-    private Animator answer;
-    private Animator header;
-    private AnimatorSet set = new AnimatorSet();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scale = getResources().getDisplayMetrics().density;
-
-        findViewById(R.id.flipCard).setCameraDistance(8000 * scale);
-
-        rotate = AnimatorInflater.loadAnimator(this, R.animator.card_flip_rotate);
-        question = AnimatorInflater.loadAnimator(this, R.animator.card_flip_text_question);
-        answer = AnimatorInflater.loadAnimator(this, R.animator.card_flip_text_answer);
-        header = AnimatorInflater.loadAnimator(this, R.animator.card_header);
-
-        rotate.setTarget(findViewById(R.id.flipCard));
-        question.setTarget(findViewById(R.id.flashcard_question));
-        answer.setTarget(findViewById(R.id.flashcard_answer));
-        header.setTarget(findViewById(R.id.headerQuestion));
-
-        header.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-                if (answerShown)
-                    ((TextView) findViewById(R.id.headerQuestion)).setText(getText(R.string.header_answer));
-                else
-                    ((TextView) findViewById(R.id.headerQuestion)).setText(getText(R.string.header_question));
-            }
-        });
-
-        set.playTogether(rotate, question, answer);
     }
 
     public void onCardClick(View v) {
         if (answerShown) {
-            rotate.setInterpolator(new ReverseOffsetInterpolator());
-            question.setInterpolator(new ReverseLinearInterpolator());
-            answer.setInterpolator(new ReverseLinearInterpolator());
+            findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            ((TextView) findViewById(R.id.headerQuestion)).setText(R.string.header_question);
         } else {
-            rotate.setInterpolator(new OffsetInterpolator());
-            question.setInterpolator(new LinearInterpolator());
-            answer.setInterpolator(new LinearInterpolator());
+            findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            ((TextView) findViewById(R.id.headerQuestion)).setText(R.string.header_answer);
         }
-        set.start();
-        header.start();
         findViewById(R.id.choiceOne).setBackground(null);
         findViewById(R.id.choiceTwo).setBackground(null);
         findViewById(R.id.choiceThree).setBackground(null);
@@ -84,22 +33,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void onCorrectChoiceClick(View v) {
         if (answerShown) return;
-        rotate.setInterpolator(new OffsetInterpolator());
-        question.setInterpolator(new LinearInterpolator());
-        answer.setInterpolator(new LinearInterpolator());
-        set.start();
-        header.start();
+        findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+        ((TextView) findViewById(R.id.headerQuestion)).setText(R.string.header_answer);
         v.setBackgroundColor(getColor(R.color.answer_correct));
         answerShown = true;
     }
 
     public void onIncorrectChoiceClick(View v) {
         if (answerShown) return;
-        rotate.setInterpolator(new OffsetInterpolator());
-        question.setInterpolator(new LinearInterpolator());
-        answer.setInterpolator(new LinearInterpolator());
-        set.start();
-        header.start();
+        findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+        ((TextView) findViewById(R.id.headerQuestion)).setText(R.string.header_answer);
         v.setBackgroundColor(getColor(R.color.answer_incorrect));
         answerShown = true;
     }
